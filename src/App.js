a9db98602ba557b6243582ba30ac28880308e11a
balemrog <emre044@hotmail.fr>
2023-08-23 17:56:06 +0200

    ajout bootstrap dep

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+
+  const [teachers, setTeachers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch("/api/teachers")
+        .then(response => response.json())
+        .then(data => {
+          setTeachers(data);
+          setLoading(false);
+        })
+  }, []);
+  console.log(teachers);
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello world
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="App-intro">
+            <h2>TEACHER List</h2>
+            {teachers.map(teacher =>
+                <div key={teacher.id}>
+                  {teacher.name}
+                </div>
+            )}
+          </div>
+        </header>
+      </div>
   );
 }
 
