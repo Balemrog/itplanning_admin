73d9445a0274818fe36dbe19e9f7e3d39332de3c
balemrog <emre044@hotmail.fr>
2023-10-22 18:52:06 +0200

    install calendar

--- src/Home.js
+++ src/Home.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import AppNavbar from './helpers/AppNavbar';
 import { Container } from 'reactstrap';
+import SearchPlanning from "./planning/SearchPlanning";
 
 const Home = () => {
     return (
         <div>
             <AppNavbar/>
             <Container fluid>
-
+                <SearchPlanning/>
             </Container>
         </div>
     );
