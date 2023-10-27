4431e46f65668a6da871f163e246357b8e3b584c
Le-romi <romeo.moreaux35@gmail.com>
2023-10-27 15:42:25 +0200

    add dropdown

--- src/planning/SearchPlanning.js
+++ src/planning/SearchPlanning.js
@@ -1,55 +1,141 @@
-import React, {useState} from 'react';
-import {Button, Card, CardBody, Col, Form, FormGroup, Input, Label, Row} from "reactstrap";
+import React, { useState } from "react";
+import {
+  Card,
+  CardBody,
+  Col,
+  Form,
+  Row,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
 
 function SearchPlanning() {
+  const initialFormState = {
+    room: ["Salle de Senol", "Salle de Semih"],
+    lesson: ["Leçon de valorant", "Leçon de cs"],
+    teacher: ["Le beau Emre", "Balemroprof"],
+  };
 
-    const initialFormState = {
-        room: '',
-        lesson: '',
-        teacher: '',
-    };
-    const [planning, setPlanning] = useState(initialFormState);
+  const [planning, setPlanning] = useState(initialFormState);
+  const [dropdownOpenSalle, setDropdownOpenSalle] = useState(false);
+  const [salle, setSalle] = useState(" ");
+  const [dropdownOpenCours, setDropdownOpenCours] = useState(false);
+  const [cours, setCours] = useState(" ");
+  const [dropdownOpenEnseignant, setDropdownOpenEnseignant] = useState(false);
+  const [enseignant, setEnseignant] = useState(" ");
 
-    const handleChange = (event) => {
-        const { name, value, type, checked } = event.target;
+  const toggleSalle = () => setDropdownOpenSalle((prevState) => !prevState);
+  const toggleCours = () => setDropdownOpenCours((prevState) => !prevState);
+  const toggleEnseignant = () =>
+    setDropdownOpenEnseignant((prevState) => !prevState);
 
-        if (type === "checkbox") {
-            setPlanning({ ...planning, [name]: checked });
-        } else {
-            setPlanning({ ...planning, [name]: value });
-        }
+  const handleChange = (event) => {
+    const { name, value, type, checked } = event.target;
+    setSalle(value);
+    console.log(salle);
+
+    if (type === "checkbox") {
+      setPlanning({ ...planning, [name]: checked });
+    } else {
+      setPlanning({ ...planning, [name]: value });
     }
+  };
 
-    return (
-        <>
-            <Row>
-                <Col>
-                    <Card className='className="mb-5"'>
-                        <CardBody>
-                            <h2>Rechercher un planning</h2>
-                            <Form>
-                                <div className='row'>
-                                    <FormGroup className="col-md-4 mb-3">
-                                        <Label for="room">Salle</Label>
-                                        <Input type="text" name="room" id="room" value={planning.room || ''} onChange={handleChange}/>
-                                    </FormGroup>
-                                    <FormGroup className="col-md-4 mb-3">
-                                        <Label for="lesson">Cours</Label>
-                                        <Input type="text" name="lesson" id="lesson" value={planning.lesson || ''} onChange={handleChange}/>
-                                    </FormGroup>
-                                    <FormGroup className="col-md-4 mb-3">
-                                        <Label for="teacher">Enseignant</Label>
-                                        <Input type="text" name="teacher" id="teacher" value={planning.teacher || ''} onChange={handleChange}/>
-                                    </FormGroup>
-                                </div>
-                                <Button color="primary" type="button" className='float-end'>Rechercher</Button>
-                            </Form>
-                        </CardBody>
-                    </Card>
-                </Col>
-            </Row>
-        </>
-    )
+  const handleSetSalle = (e) => {
+    let roomName = e.target.getAttribute("roomName");
+    setSalle(roomName);
+  };
+
+  const handleSetCours = (e) => {
+    let coursName = e.target.getAttribute("coursName");
+    setCours(coursName);
+  };
+
+  const handleSetEnseignant = (e) => {
+    console.log(e.target.getAttribute("enseignantName"));
+    let enseignantName = e.target.getAttribute("enseignantName");
+    setEnseignant(enseignantName);
+  };
+
+  return (
+    <>
+      <Row>
+        <Col>
+          <Card className='className="mb-5"'>
+            <CardBody>
+              <h2>Rechercher un planning</h2>
+              <Form>
+                <div className="d-flex p-5 justify-content-center">
+                  <div className="d-flex p-5">
+                    <Dropdown
+                      isOpen={dropdownOpenSalle}
+                      toggle={toggleSalle}
+                      direction={"down"}
+                    >
+                      <DropdownToggle caret>
+                        {salle == " " ? "Votre salle" : salle}
+                      </DropdownToggle>
+                      <DropdownMenu>
+                        <DropdownItem header>Votre salle</DropdownItem>
+                        {initialFormState.room.map((s) => (
+                          <DropdownItem onClick={handleSetSalle} roomName={s}>
+                            {s}
+                          </DropdownItem>
+                        ))}
+                      </DropdownMenu>
+                    </Dropdown>
+                  </div>
+                  <div className="d-flex p-5">
+                    <Dropdown
+                      isOpen={dropdownOpenCours}
+                      toggle={toggleCours}
+                      direction={"down"}
+                    >
+                      <DropdownToggle caret>
+                        {cours == " " ? "Votre cours" : cours}
+                      </DropdownToggle>
+                      <DropdownMenu>
+                        <DropdownItem header>Votre cours</DropdownItem>
+                        {initialFormState.lesson.map((c) => (
+                          <DropdownItem onClick={handleSetCours} coursName={c}>
+                            {c}
+                          </DropdownItem>
+                        ))}
+                      </DropdownMenu>
+                    </Dropdown>
+                  </div>
+                  <div className="d-flex p-5">
+                    <Dropdown
+                      isOpen={dropdownOpenEnseignant}
+                      toggle={toggleEnseignant}
+                      direction={"down"}
+                    >
+                      <DropdownToggle caret>
+                        {enseignant == " " ? "Votre enseignant" : enseignant}
+                      </DropdownToggle>
+                      <DropdownMenu>
+                        <DropdownItem header>Votre enseignant</DropdownItem>
+                        {initialFormState.teacher.map((e) => (
+                          <DropdownItem
+                            onClick={handleSetEnseignant}
+                            enseignantName={e}
+                          >
+                            {e}
+                          </DropdownItem>
+                        ))}
+                      </DropdownMenu>
+                    </Dropdown>
+                  </div>
+                </div>
+              </Form>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 }
 
 export default SearchPlanning;
